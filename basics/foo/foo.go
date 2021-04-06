b9fa27b69f910b73bd48ad40ed532384612c56c4
Suhas K <suhas.ksv@gmail.com>
2021-04-06 18:56:38 +0530

    ./push.sh

--- basics/foo/foo.go
+++ basics/foo/foo.go
@@ -1,4 +1,5 @@
 package main // Ever Go source code starts with package
+
 import "fmt"
 
 /*
@@ -82,15 +83,72 @@ func main() {
 	//
 	//a %= b
 	//fmt.Println(a)
+	//
+	//var a int16 = 4
+	//b := &a
+	//fmt.Println(b)
+	//*b = 7
+	//fmt.Println(a)
+	//
+	//var arr = []uint{1, 2, 3, 4, 5, 6}
+	//for i, j := range arr {
+	//	fmt.Println(i, j)
+	//}
 
-	var a int16 = 4
-	b := &a
-	fmt.Println(b)
-	*b = 7
-	fmt.Println(a)
+	//for i, j := range "golang" {
+	//	fmt.Printf("The index at %U is %d\n", j, i)
+	//}
 
+	var map1 = map[string]string{"Go": ".go", "Python": ".py", "Java": ".java", "Unix exec": ".exec", "App": ".app", "compiled c unix": ".so"}
 
+	for i, j := range map1 {
+		fmt.Printf("%s --> %s\n", i, j)
+	}
+
+	chn1 := make(chan int)
+	go func() {
+		chn1 <- 10
+		chn1 <- 100
+		chn1 <- 1000
+		chn1 <- 10000
+		close(chn1)
+	}()
+
+	for i := range chn1 {
+		fmt.Println(i)
+	}
+	switch day := 4; day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	}
+	var i int = 2
+	switch {
+	case i == 1:
+		fmt.Println("Python")
+	case i == 2:
+		fmt.Println("Go")
+	case i == 3:
+		fmt.Println("Java")
+	case i == 4:
+		fmt.Println("C")
+	case i == 5:
+		fmt.Println("")
+	}
 }
+
+
 /*
 func foo() {
 	years := []int{10, 100, 200, 2000, 20}
---
+++ quadratic-equation.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+)
+
+var (
+	a     float64
+	b     float64
+	c     float64
+	d     float64
+	alpha float64
+	beta  float64
+	i     int
+	ask   string
+)
+
+func main() {
+	fmt.Print("Enter the value of a: ")
+	fmt.Scan(&a)
+	
+	fmt.Print("Enter the value of b: ")
+	fmt.Scan(&b)
+
+	fmt.Print("Enter the value of c:" )
+	fmt.Scan(&c)
+	i = dChk(a, b, c)
+	alpha, beta = calcAlphaBeta(a, b, c)
+	switchStatement(i, alpha, beta)
+	callAgain()
+}
+
+func dChk(a, b, c float64) int {
+	d = (b * b) - (4 * a * c)
+
+	if d > 0 {
+		i = 1
+	} else if d == 0 {
+		i = 2
+	} else if d < 0 {
+		i = 3
+	}
+	return i
+}
+
+func calcAlphaBeta(a, b, c float64) (float64, float64) {
+	d = (b * b) - (4 * a * c)
+	alpha = (-b + math.Sqrt(d)) / (2 * a)
+	beta = (-b - math.Sqrt(d)) / (2 * a)
+	return alpha, beta
+}
+
+func switchStatement(i int, alpha, beta float64) {
+	switch i {
+	case 1:
+		fmt.Printf("\nRoots are Real\nThe value of alpha = %.2f and beta = %.2f\n", alpha, beta)
+	case 2:
+		fmt.Printf("\nRoots are Real and equal\nThe value of alpha and beta = %.2f\n", alpha)
+	case 3:
+		fmt.Printf("\nRoots are imaganary\nThe value of alpha = %.2f and beta = %.2f\n", alpha, beta)
+	}
+}
+
+func callAgain() {
+	fmt.Print("Do you want to calculate Roots of another quadratic equation? type 'y' to continue: ")
+	fmt.Scan(&ask)
+	if ask == "y" {
+		main()
+	} else {
+		os.Exit(0)
+	}
+}
