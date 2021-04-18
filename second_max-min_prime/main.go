aef4b08d63e39100e9dea66755768d299667b5e4
Suhas K <suhas.ksv@gmail.com>
2021-04-18 15:43:08 +0530

    Successfully completed 2nd highest and lowest numbers in an array

--- second_max-min_prime/main.go
+++ second_max-min_prime/main.go
@@ -1,49 +1,46 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	var n int
-	var min int
-	var max int
-	var arr []int
-
-	fmt.Print("Enter no of test cases: ")
-	_, _ = fmt.Scan(&n)
-
-	for i := 0; i < n; i++ {
-		fmt.Print("Enter minimum and maximum value: ")
-		_, _ = fmt.Scan(&min, &max)
-
-		for j := min; j <= max; j++ {
-			if isPrime(j) {
-				arr = append(arr, j)
-			}
-		}
-
-		if len(arr) == 0 {
-			fmt.Println(-1)
-		} else if min == max {
-			fmt.Println(0)
-		} else {
-			fmt.Println(arr[len(arr)-1] - arr[1])
-		}
-	}
+	var arr = []int{10, 20, 400, 300, 80000, 100, 200, 90, 1}
+	fmt.Println("Given array:", arr)
+	fmt.Println("Second Highest number in the given array:", _2max(arr))
+	fmt.Println("Second Lowest number in the given array:" , _2min(arr))
 }
 
-func isPrime(a int) bool {
-	if a != 2 && a%2 == 0 {
-		return false
-	}
-	for i := 2; i <= int(math.Sqrt(float64(a))); i++ {
-		if a%i == 0 {
-			return false
+func _2max(arr []int) int {
+	max1 := 0
+	max := 0
+	
+	for _, i := range arr {
+		if i > max {	
+			if max1 < max {
+				max1 = max
+			}
+			max = i
+		} else if i > max1 {
+			max1 = i
 		}
 	}
-	return true
+	return max1
+}
+
+func _2min(arr []int) int {
+	var min1 = arr[0]
+	min := arr[0]
+
+	for _, i := range arr {
+		if i < min {
+			if min1 > min {
+				min1 = min
+			}
+			min = i
+		} else if i < min1 {
+			min1 = i
+		}
+	}
+	return min1
 }
 
 /*
