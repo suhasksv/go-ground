aff781d5a127bb5a085c1eaba8b1e9f5c60b49aa
Suhas K <suhas.ksv@gmail.com>
2022-01-21 11:36:38 +0530

    Advent of Code 2015: Completed Day4 Part 1 + 2

---
+++ advent-of-code/2015/day4/main2.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+"crypto/md5"
+"fmt"
+"io"
+"regexp"
+)
+
+func main() {
+	input := "iwrupvqb"
+	cnt := 0
+	regp := regexp.MustCompile("\\A000000")
+	mine := ""
+
+	for !regp.MatchString(mine) {
+		cnt++
+		hashgen := md5.New()
+		_, err := io.WriteString(hashgen, input)
+		if err != nil {
+			return
+		}
+		_, err = io.WriteString(hashgen, fmt.Sprintf("%d", cnt))
+		if err != nil {
+			return
+		}
+		mine = fmt.Sprintf("%x", hashgen.Sum(nil))
+	}
+	println(cnt)
+}
+
